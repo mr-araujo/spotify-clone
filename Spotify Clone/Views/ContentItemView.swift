99221d422839325dd0fed8544ff18ba4e988bfdc
Murillo R. Araujo <mr_araujo@icloud.com>
2020-04-30 21:24:34 -0300

    Add layer 2

--- Spotify Clone/Views/ContentItemView.swift
+++ Spotify Clone/Views/ContentItemView.swift
@@ -11,13 +11,31 @@ import SwiftUI
 struct ContentItemView: View {
     var body: some View {
         ZStack {
+            LinearGradient(gradient:
+            Gradient(colors: [
+                Color.black.opacity(0.05),
+                Color.black.opacity(0.05),
+                Color.black.opacity(0.05),
+                Color.black,
+                Color.black
+            ]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
             VStack {
                 Spacer()
                     .frame(height: 50)
                 
-                Image(systemName:"music.note")
-                Text("Title")
+                Image("music")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                
+                Text("Pop Korea")
+                    .foregroundColor(.black)
+                    .font(.system(size: 24, weight: .bold))
+                
                 Text("Subtitle")
+                    .foregroundColor(.black)
+                    .font(.system(size: 16, weight: .medium))
                 
                 Spacer()
             }
@@ -26,20 +44,32 @@ struct ContentItemView: View {
                 VStack(spacing:0) {
                     HStack {
                         Spacer()
-                            .frame(height: 200)
-                            .background(Color.red.opacity(0.3))
+                            .frame(height: 400)
+                            .background(LinearGradient(gradient:
+                                Gradient(colors: [
+                                    Color.clear,
+                                    Color.clear,
+                                    Color.clear,
+                                    Color.clear,
+                                    Color.black
+                                ]), startPoint: .top, endPoint: .bottom))
                     }
                     
                     VStack {
                         ForEach(0..<30) { index in
                             HStack {
-                                Text("\(index)")
-                                    .foregroundColor(.white)
+                                Image(systemName: "pause")
+                                    .padding(.trailing, 10)
+                                Text("Album track \(index + 1)")
                                 Spacer()
+                                Image(systemName: "ellipsis")
                             }
+                            .padding(15)
+                            .background(Color.black)
+                            .foregroundColor(.white)
                         }
                     }.background(Color.black)
-                }.background(Color.yellow.opacity(0.2))
+                }.background(Color.clear)
             }
         }
     }
